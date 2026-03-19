a80ce689624ce443441c67c71b8670846766f6ed
Ania-su <antsaaa.randria@gmail.com>
2026-03-19 10:58:04 +0300

    feat: Q°A) TD3 welcome http responses

--- src/main/java/k2/example/httpresponse/controller/WelcomeController.java
+++ src/main/java/k2/example/httpresponse/controller/WelcomeController.java
@@ -1,6 +1,8 @@
 package k2.example.httpresponse.controller;
 
 import k2.example.httpresponse.entity.Welcome;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,11 @@ public class WelcomeController {
     private static final String template = "Welcome %s!";
 
     @GetMapping("/welcome")
-    public Welcome welcome(@RequestParam(defaultValue = "") String name) {
-        return new Welcome(String.format(template, name));
+    public ResponseEntity<Welcome> welcome(@RequestParam(required = false) String name) {
+        if (name == null || name.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        Welcome welcome = new Welcome(String.format(template, name));
+        return ResponseEntity.ok(welcome);
     }
 }
