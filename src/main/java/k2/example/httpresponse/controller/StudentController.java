0bd83410704d51791c0b347dae1b07ab4aab8b04
Ania-su <antsaaa.randria@gmail.com>
2026-03-23 22:48:16 +0300

    change: Q°C) TD3 student get code http response

--- src/main/java/k2/example/httpresponse/controller/StudentController.java
+++ src/main/java/k2/example/httpresponse/controller/StudentController.java
@@ -1,12 +1,14 @@
 package k2.example.httpresponse.controller;
 
 import k2.example.httpresponse.entity.Student;
+import k2.example.httpresponse.repository.StudentRepository;
 import k2.example.httpresponse.service.StudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 public class StudentController {
@@ -28,10 +30,32 @@ public class StudentController {
     }
 
     @GetMapping("/students")
-    public String getStudents(@RequestHeader(value = "Accept", defaultValue = "text/plain") String accept) {
-        if (!accept.contains("text/plain")) {
-            return "Unsupported Format";
+    public ResponseEntity<String> getStudents(@RequestHeader(value = "Accept", defaultValue = "text/plain") String accept) {
+        StudentRepository repository = new StudentRepository();
+        try {
+            if (accept == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing Accept Header");
+            }
+
+            if (accept.equals("text/plain")) {
+                StringBuilder names = new StringBuilder();
+
+                repository.getAllStudents().forEach(s ->
+                        names.append(s.getFirstName()).append(" ").append(s.getLastName()).append("\n")
+                );
+                return ResponseEntity.ok().body(names.toString());
+
+            }
+            else if (accept.equals("application/json")) {
+                List<Student> students = repository.getAllStudents();
+                return ResponseEntity.ok(students.toString());
+            }
+            else {
+                return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
+                        .body("Unsupported Format");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Server Error");
         }
-        return service.getStudentNames();
     }
 }
